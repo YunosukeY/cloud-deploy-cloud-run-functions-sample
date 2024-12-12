0cbb1d72abe7d5fac69d6c9e9fbb5816b587d612
Yunosuke Yamada <yunosuke.yamada@3-shake.com>
2024-12-12 15:11:55 +0900

    initial deploy

---
+++ hello_world.go
@@ -0,0 +1,16 @@
+package helloworld
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+)
+
+func init() {
+	functions.HTTP("HelloGet", helloGet)
+}
+
+func helloGet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello, World!")
+}
